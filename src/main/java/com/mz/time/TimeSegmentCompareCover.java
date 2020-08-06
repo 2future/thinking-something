a28945a78cb6c9872fe06f0d984a58d990400c31
mz <mz490848173@163.com>
2020-08-06 10:41:02 +0800

    docs(添加uml类图): 添加uml类图
    添加uml类图

--- src/main/java/com/mz/time/TimeSegmentCompareCover.java
+++ src/main/java/com/mz/time/TimeSegmentCompareCover.java
@@ -39,16 +39,16 @@ public class TimeSegmentCompareCover {
             if (timeTemp.isClean()) {
                 timeTemp.coverTemp(convertToTimeStamp(timeNode));
             } else {
-                long partFirDiff = Math.abs(timeTemp.getTemp()-(timeTemp.getTemp() - convertToTimeStamp(timeNode))/2);
+                long partFirDiff = Math.abs(timeTemp.getTemp() - (timeTemp.getTemp() - convertToTimeStamp(timeNode)) / 2);
                 timeTemp.clean();
                 List<Date> timeNodes = partSed.getTimeNodes();
                 for (Date timeNodeSed : timeNodes) {
                     if (timeTemp.isClean()) {
                         timeTemp.coverTemp(convertToTimeStamp(timeNodeSed));
                     } else {
-                        long partSedDiff = Math.abs(timeTemp.getTemp()-(timeTemp.getTemp() - convertToTimeStamp(timeNodeSed))/2);
+                        long partSedDiff = Math.abs(timeTemp.getTemp() - (timeTemp.getTemp() - convertToTimeStamp(timeNodeSed)) / 2);
                         //中间点 值差
-                        long middleAbs = Math.abs(partFirDiff  - partSedDiff );
+                        long middleAbs = Math.abs(partFirDiff - partSedDiff);
                         return (middleAbs < (partFir.getDiffAbs() / 2 + partSed.getDiffAbs() / 2));
                     }
                 }
@@ -60,29 +60,52 @@ public class TimeSegmentCompareCover {
     /**
      * 临时时间点
      */
-   private class TimeTemp {
+    private class TimeTemp {
 
         private Long temp;
 
+        /**
+         * 覆盖时间临时点
+         *
+         * @param timeStamp
+         */
         public void coverTemp(Long timeStamp) {
             temp = timeStamp;
         }
 
+        /**
+         * 获取当前临时点
+         *
+         * @return
+         */
         public Long getTemp() {
             Objects.requireNonNull(temp);
-            return temp;
+            return new Long(temp);
         }
 
+        /**
+         * 判断当前是否有临时时间点
+         *
+         * @return
+         */
         public boolean isClean() {
             return temp == null;
         }
 
+        /**
+         * 清空 当前临时时间点
+         */
         public void clean() {
             temp = null;
         }
 
     }
 
+    /**
+     * 内部方法 date类型转时间戳
+     * @param date
+     * @return
+     */
     private long convertToTimeStamp(Date date) {
         Objects.requireNonNull(date);
         return date.getTime();
--- src/test/java/com/mz/compress/CompressTest.java
+++ src/test/java/com/mz/compress/CompressTest.java
@@ -1,6 +1,7 @@
 package com.mz.compress;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.time.StopWatch;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -18,12 +19,19 @@ public class CompressTest {
 
     @Test
     void testCompressSiz() throws IOException {
+        StopWatch stopWatch=new StopWatch();
+        stopWatch.start();
         File file = new File("/Users/chaojitiangou/Documents/学习/Xxljob学习记录.docx");
+        stopWatch.split();
+        long splitTime = stopWatch.getSplitNanoTime();
+        System.err.println(String.format("读取文件时间:%s毫秒",splitTime/1000));
         System.err.println(String.format("压缩前文件大小:%s",FileUtils.readFileToByteArray(file).length));
         CompressDemo demo=new CompressDemo(file);
         demo.compressFIle();
         byte[] compressFIleByte = demo.getCompressFIleByte();
         System.err.println(String.format("压缩后文件大小:%s",compressFIleByte.length));
+        stopWatch.split();
+        System.err.println(String.format("读取文件时间:%s毫秒",stopWatch.getSplitNanoTime()/1000));
     }
 
 }
